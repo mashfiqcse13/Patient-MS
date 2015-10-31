b44cddd4eb6ed4cca271b7296603461b690d9a78
mashfiqcse13 <MD. Mashfiq@DESKTOP-EARQJ11>
2015-10-31 12:46:58 +0600

    Menu Update v1

--- application/views/Themes/admin/startbootstrap-sb-admin-1.0.3/section-menu.php
+++ application/views/Themes/admin/startbootstrap-sb-admin-1.0.3/section-menu.php
@@ -3,7 +3,7 @@
  * Sidebar Menu Items - These collapse to the responsive navigation menu on small screens
  */
 ?>
-<div class="collapse navbar-collapse navbar-ex1-collapse">
+<!--<div class="collapse navbar-collapse navbar-ex1-collapse">
     <ul class="nav navbar-nav side-nav">
         <li>
             <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
@@ -39,6 +39,28 @@
         </li>
         <li>
             <a href="index-rtl.html"><i class="fa fa-fw fa-dashboard"></i> RTL Dashboard</a>
+        </li>
+    </ul>
+</div>-->
+<div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav side-nav">
+        <li>
+            <a href="#"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+        </li>
+        <li>
+            <a href="javascript:;" data-toggle="collapse" data-target="#demo">
+                <i class="fa fa-fw fa-arrows-v"></i> 
+                Patient Section 
+                <i class="fa fa-fw fa-caret-down"></i>
+            </a>
+            <ul id="demo" class="collapse">
+                <li>
+                    <a href="<?= site_url('admin/patient/add')?>">Patient Entry</a>
+                </li>
+                <li>
+                    <a href="<?= site_url('admin/patient')?>">Patient Management</a>
+                </li>
+            </ul>
         </li>
     </ul>
 </div>
